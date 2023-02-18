66d1fa269b562fb86b840a1ae5218db04b879fbf
Hamza Aslam <hamzaaslam016@gmail.com>
2023-02-19 00:10:25 +0500

    added back button and link in header and footer

--- resources/views/includes/landing/footer.blade.php
+++ resources/views/includes/landing/footer.blade.php
@@ -5,7 +5,7 @@
                                             <div class="col-md-2">
                                                
                                                 <div class="logo">
-                                                     <img src="{{url('logo/Speak_2_Impact_Logo_Black_Bg_white.png')}}" height="80px" class="css-class" alt="alt text">
+                                                     <a href="{{url('/home')}}"><img src="{{url('logo/Speak_2_Impact_Logo_Black_Bg_white.png')}}" height="80px" class="css-class cursor-pointer" alt="alt text"></a>
                                                 </div>
                                                 
                                                 
