77e12880e274165ebd1d8b027e1108bbd243ac2f
irtechweb <amber@irtechweb.com>
2023-03-14 00:12:15 +0000

    payment detail pages ui fixed -- partial

--- resources/views/payment-details.blade.php
+++ resources/views/payment-details.blade.php
@@ -29,49 +29,61 @@
                             <form id="payment-form" method="post" enctype="multipart/form-data" action="#"> 
                                 @csrf
                                 <div id="card-element">
-                                <div class="membership-field card">
-                                    <label for="exampleInputEmail1" class="form-label">Card number</label>
-                                    <input type="text" class="form-control f-img card-number" placeholder="Enter card number" name='card_number' required> <img src="{{asset('/images/credit_card.svg')}}" alt="">
-                                    <div class="master-card">
-                                        <a href="#"><img src="{{asset('/images/visa.svg')}}" alt=""></a>
-                                        <a href="#"><img src="{{asset('/images/m-card.svg')}}" alt=""></a>
+                                    <div class="membership-field card">
+                                        <label for="exampleInputEmail1" class="form-label">Card number</label>
+                                        <input type="text" class="form-control f-img card-number" placeholder="Enter card number" name='card_number' required> <img src="{{asset('/images/credit_card.svg')}}" alt="">
+                                        <div class="master-card">
+                                            <a href="#"><img src="{{asset('/images/visa.svg')}}" alt=""></a>
+                                            <a href="#"><img src="{{asset('/images/m-card.svg')}}" alt=""></a>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="row">
-                                    <div class="membership-field m-half">
-                                        <label for="name" class="form-label">Name on Card</label>
-                                        <input name='card_name' type="text" placeholder="Name on Card" class="form-control" required> </div>
-                                    <div class="membership-field m-half cvc">
-                                        <label for="exampleInputEmail1" class="form-label">CVV</label>
-                                        <input type="text" name='cvc' class="form-control card-cvc" placeholder="Enter CVV"> </div>
-                                </div>
-                                <div class="row">
-                                    <div class="membership-field m-half expiration">
-                                        <label for="expiryMonth" class="form-label">Expiry Month</label>
-                                        <input name='expiry_month' type="text" class="form-control card-expiry-month" placeholder="MM" required> </div>
-                                    <div class="membership-field m-half expiration">
-                                        <label for="expiryYear" class="form-label">Expiry Year</label>
-                                        <input name='expiry_year' type="text" class="form-control card-expiry-year" placeholder="YYYY" required> </div>
-                                </div>
-                                <div class="devider-line"></div>
-                                <div class="row" style="margin-bottom: 5%;">
-                                    <div class="membership-field m-half"> <span for="exampleInputEmail1" style="font-weight: bold; color: black; font-size: 18px;" class="form-label">Total</span> </div>
-                                    <div class="membership-field m-half"> 
-                                        <span for="exampleInputEmail1" style="float: right; font-weight: bold; color: #1C1C1C; font-size: 18px;" class="form-label">£@if(isset($subscription['price'])) 
-                                                      @php 
-                                                      //$discount = $subscription['price']/100*$subscription['discount_percentage'];
-                                                      @endphp
-                                                      {{-- {{$subscription['price']-$discount}} --}}
-                                                      {{$subscription['price']}}
-                                                        @endif 
-                                                        per 
-                                                    @if(isset($subscription['plans'])) 
-                                                    {{ $subscription['plans'] == 'halfyearly' ? 'half y early' : $subscription['plans']}} 
-                                                    @endif
-                                        </span> 
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="membership-field">
+                                                <label for="name" class="form-label">Name on Card</label>
+                                                <input name='card_name' type="text" placeholder="Name on Card" class="form-control" required> 
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="membership-field cvc">
+                                                <label for="exampleInputEmail1" class="form-label">CVV</label>
+                                                <input type="text" name='cvc' class="form-control card-cvc" placeholder="Enter CVV"> 
+                                            </div>
+                                        </div>    
                                     </div>
-                                </div>
-                                <button type="submit" class="m-payment" style="background-color: #1C1C1C; color: #fff;"> Start membership</button>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="membership-field expiration">
+                                                <label for="expiryMonth" class="form-label">Expiry Month</label>
+                                                <input name='expiry_month' type="text" class="form-control card-expiry-month" placeholder="MM" required> 
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="membership-field expiration">
+                                            <label for="expiryYear" class="form-label">Expiry Year</label>
+                                            <input name='expiry_year' type="text" class="form-control card-expiry-year" placeholder="YYYY" required> 
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="devider-line"></div>
+                                    <div class="row" style="margin-bottom: 5%;">
+                                        <div class="membership-field m-half"> <span for="exampleInputEmail1" style="font-weight: bold; color: black; font-size: 18px;" class="form-label">Total</span> </div>
+                                        <div class="membership-field m-half"> 
+                                            <span for="exampleInputEmail1" style="float: right; font-weight: bold; color: #1C1C1C; font-size: 18px;" class="form-label">£@if(isset($subscription['price'])) 
+                                                        @php 
+                                                        //$discount = $subscription['price']/100*$subscription['discount_percentage'];
+                                                        @endphp
+                                                        {{-- {{$subscription['price']-$discount}} --}}
+                                                        {{$subscription['price']}}
+                                                            @endif 
+                                                            per 
+                                                        @if(isset($subscription['plans'])) 
+                                                        {{ $subscription['plans'] == 'halfyearly' ? 'half y early' : $subscription['plans']}} 
+                                                        @endif
+                                            </span> 
+                                        </div>
+                                    </div>
+                                     <button type="submit" class="m-payment" style="background-color: #1C1C1C; color: #fff;"> Start membership</button>
                                 </div>
                             </form>
                         </div>
@@ -79,13 +91,15 @@
                     <!--                            <input type="button" name="previous" onclick="history.go(-1);" class="previous action-button-previous" value="Previous" />-->
                 </fieldset>
             </form>
-            <div class="form-card col-sm-12 col-md-12 col-lg-12">
-                <div class="membership-payment">
-                    <form method="post" action="{{route('postPaymentWithpaypal')}}" enctype="multipart/form-data" id="msform"> @csrf
-                        <input type="hidden" name='user_id' value="{{Crypt::encrypt($data['user_id'])}}">
-                        <input type="hidden" name='subscription_id' value="{{Crypt::encrypt($data['subscription_id'])}}">
-                        <button type="submit" class="m-payment"><img src="{{asset('/images/pp.svg')}}" alt=""> Pay with Paypal</button>
-                    </form>
+            <div class="row">
+                <div class="form-card col-lg-12 col-md-6 col-sm-12">
+                    <div class="membership-payment">
+                        <form method="post" action="{{route('postPaymentWithpaypal')}}" enctype="multipart/form-data" id="msform"> @csrf
+                            <input type="hidden" name='user_id' value="{{Crypt::encrypt($data['user_id'])}}">
+                            <input type="hidden" name='subscription_id' value="{{Crypt::encrypt($data['subscription_id'])}}">
+                            <button type="submit" class="m-payment"><img src="{{asset('/images/pp.svg')}}" alt=""> Pay with Paypal</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
